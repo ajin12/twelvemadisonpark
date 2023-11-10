df0a5a85a2c56ea34704fda4a1b97eaf43fd631d
Amy Jin <amy_jin@college.harvard.edu>
2023-11-10 18:12:53 -0500

    Update button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-const ButtonContainer = styled.div`
-  display: flex;
-  border: 1px solid white;
-  padding: 6px 12px;
-  cursor: pointer;
-`;
-
-const ButtonText = styled.div`
+const StyledButton = styled.button`
   color: white;
   font-family: "Cormorant";
   font-size: 16px;
   font-style: italic;
+  background: transparent;
+  cursor: pointer;
+  border: 1px solid white;
+  padding: 6px 12px;
 `;
 
 function Button({ text }) {
-  return (
-    <ButtonContainer>
-      <ButtonText>{text}</ButtonText>
-    </ButtonContainer>
-  );
+  return <StyledButton>{text}</StyledButton>;
 }
 
 export default Button;
